ef3d7f5a4bf6053f39a00789367c211883af1383
Nada El Hakim <elhakim.nada88@gmail.com>
2018-08-13 11:14:56 +0200

    Added reactstrap

---
+++ app/src/pages/Dashboard/components/TransactionForm/TradePasswordModal.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class TradePasswordModal extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default TradePasswordModal;
--- app/src/pages/Dashboard/components/TransactionForm/TransactionForm.js
+++ app/src/pages/Dashboard/components/TransactionForm/TransactionForm.js
@@ -5,14 +5,30 @@ import { I18n } from 'react-i18next';
 import {Combobox} from 'react-widgets';
 import SendTransactionActions, { SendTransactionSelectors} from './SendTransactionRedux';
 import {SettingsSelectors} from "../../pages/Settings/SettingsRedux";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 // import 'react-widgets/dist/css/react-widgets.css';
 
 
 class TransactionForm extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
+        this.toggle = this.toggle.bind(this);
+    }
     componentDidMount() {
         this.props.getAddressList();
     }
+
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
 
     // TODO: Save address to local storage
     submitHandler = (values) => {
@@ -30,6 +46,25 @@ class TransactionForm extends Component {
                               valueField={valueField}
                               placeholder={placeholder}
                               textField={textField} />);
+    }
+
+    renderTradePasswordModal() {
+        return (
+        <div>
+            <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+
+        );
     }
 
 
