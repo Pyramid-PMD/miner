1600410b5286d5a7338f7997335473108590a0ff
Nada El Hakim <elhakim.nada88@gmail.com>
2018-08-13 10:54:46 +0200

    Added disk id to registration

--- app/src/pages/Dashboard/pages/Overview/OverviewSaga.js
+++ app/src/pages/Dashboard/pages/Overview/OverviewSaga.js
@@ -3,12 +3,14 @@ import OverviewActions from './OverviewRedux';
 
 export function * getOverview(api, action) {
     const res = yield call(api.getOverview);
-    console.log('overview res', res);
     let errorMsg;
-    switch (res.data.code) {
-        case "0":
-            console.log('overview success', res.data.data);
-            yield put(OverviewActions.overviewSuccess(res.data.data));
-            break;
+    if (res) {
+        switch (res.data.code) {
+            case "0":
+                console.log('overview success', res.data.data);
+                yield put(OverviewActions.overviewSuccess(res.data.data));
+                break;
+        }
     }
+
 }
--- app/src/pages/Register/RegisterSaga.js
+++ app/src/pages/Register/RegisterSaga.js
@@ -6,36 +6,41 @@ import i18n from '../../config/i18n/i18next.client.config';
 
 export function * registerSaga(api, action) {
     const { user } = action;
+    console.log(user);
     const res = yield call(api.register, user);
-    if (res.data.code === "0") {
-        yield call(replace('/login'));
-        yield put(RegisterActions.registerSuccess());
-    } else {
-        let errorMsg;
-        switch (res.data.code) {
-            case -3:
-                errorMsg = i18n.t('auth:register.errors.wrongRepeatPass');
-                break;
-            case -4:
-                errorMsg = i18n.t('auth:register.errors.wrongPasswordFormat');
-                break;
-            case -5:
-                errorMsg = i18n.t('auth:register.errors.emailNotVerified');
-                break;
-            case -6:
-                errorMsg = i18n.t('auth:register.errors.verificationCodeExpired');
-                break;
-            case -7:
-                errorMsg = i18n.t('auth:register.errors.wrongVerificationCode');
-                break;
-            case -8:
-                errorMsg = i18n.t('auth:register.errors.registrationError');
-                break;
-            case -13:
-                errorMsg = i18n.t('auth:register.errors.emailRegistered');
-                break;
+    if (res) {
+        if (res.data.code === "0") {
+            yield put(replace('/login'));
+            yield put(RegisterActions.registerSuccess());
+        } else {
+            let errorMsg;
+            switch (res.data.code) {
+                case -3:
+                    errorMsg = i18n.t('auth:register.errors.wrongRepeatPass');
+                    break;
+                case -4:
+                    errorMsg = i18n.t('auth:register.errors.wrongPasswordFormat');
+                    break;
+                case -5:
+                    errorMsg = i18n.t('auth:register.errors.emailNotVerified');
+                    break;
+                case -6:
+                    errorMsg = i18n.t('auth:register.errors.verificationCodeExpired');
+                    break;
+                case -7:
+                    errorMsg = i18n.t('auth:register.errors.wrongVerificationCode');
+                    break;
+                case -8:
+                    errorMsg = i18n.t('auth:register.errors.registrationError');
+                    break;
+                case -13:
+                    errorMsg = i18n.t('auth:register.errors.emailRegistered');
+                    break;
+                case -16:
+                    // Machine has a user
+                    errorMsg = i18n.t('auth:register.errors.machineHasUser');
+            }
+            yield put(RegisterActions.registerFailure(errorMsg));
         }
-        console.log(errorMsg);
-        yield put(RegisterActions.registerFailure(errorMsg));
     }
 }
