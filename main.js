97eab75056e5e26591646d9fab4948e73d0f3756
Nada El Hakim <elhakim.nada88@gmail.com>
2018-11-23 11:21:54 +0200

    Removed fake tx

--- main.js
+++ main.js
@@ -62,24 +62,25 @@ async function sendEncryptedDiskInfoToRenderer()  {
         if (process.platform === 'win32' && process.env.NODE_ENV !== 'development') {
             const { runEncryption } = require('./encryption');
             encryptedDiskInfo = await runEncryption();
-        } else {
-            encryptedDiskInfo = `D9E3DC65EA66760E4D0DC12D4B5C759B0C129243C2391C166A2EFB3F84C8F6752C7D578F7E61E278C76A651A20B8CC742170FB00DC7C27AB5C3C5FDB8B823DFEF39AE82E4D38E8F3`;
         }
+        // else {
+        //     encryptedDiskInfo = `D9E3DC65EA66760E4D0DC12D4B5C759B0C129243C2391C166A2EFB3F84C8F6752C7D578F7E61E278C76A651A20B8CC742170FB00DC7C27AB5C3C5FDB8B823DFEF39AE82E4D38E8F3`;
+        // }
 
         console.log('encrypted disk', encryptedDiskInfo);
     } catch (error) {
 
         console.log(error);
-        const dialogOpts = {
-            type: 'info',
-            title: 'Encryption error',
-            message: JSON.stringify(error),
-            detail: 'Encryption error'
-        };
+        // const dialogOpts = {
+        //     type: 'info',
+        //     title: 'Encryption error',
+        //     message: JSON.stringify(error),
+        //     detail: 'Encryption error'
+        // };
+        //
+        // dialog.showMessageBox(dialogOpts);
 
-        dialog.showMessageBox(dialogOpts);
-
-        encryptedDiskInfo = `D9E3DC65EA66760E4D0DC12D4B5C759B0C129243C2391C166A2EFB3F84C8F6752C7D578F7E61E278C76A651A20B8CC742170FB00DC7C27AB5C3C5FDB8B823DFEF39AE82E4D38E8F3`;
+        // encryptedDiskInfo = `D9E3DC65EA66760E4D0DC12D4B5C759B0C129243C2391C166A2EFB3F84C8F6752C7D578F7E61E278C76A651A20B8CC742170FB00DC7C27AB5C3C5FDB8B823DFEF39AE82E4D38E8F3`;
     }
 
     ipcMain.on('encryption', (event, data) => {
